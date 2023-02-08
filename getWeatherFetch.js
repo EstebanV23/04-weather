e30c789d12cd3951086d20b1785e45b58a9fd142
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-02-08 14:18:37 -0500

    Cmabiando el json por la api real

--- getWeatherFetch.js
+++ getWeatherFetch.js
@@ -4,17 +4,17 @@ const BASE_URL_API = 'https://api.tomorrow.io/v4/weather/forecast'
 
 // ?location=42.3478%2C%20-71.0466&fields=temperature&units=metric&timesteps=1d&startTime=now&endTime=nowPlus120h&timezone=America%2FBogota&apikey=zWtp6tnljgVxiimFXp7kB0TVV3VnbulN
 
-export async function fetchWeatherForZone (zone = DEFAULT_ZONE) {
-  return (
-    fetch('./forecast.json')
-      .then(response => response.json())
-      .then(data => data)
-  )
-}
 // export async function fetchWeatherForZone (zone = DEFAULT_ZONE) {
 //   return (
-//     fetch(`${BASE_URL_API}?location=${zone}&apikey=${API_KEY}`)
+//     fetch('./forecast.json')
 //       .then(response => response.json())
 //       .then(data => data)
 //   )
 // }
+export async function fetchWeatherForZone (zone = DEFAULT_ZONE) {
+  return (
+    fetch(`${BASE_URL_API}?location=${zone}&apikey=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => data)
+  )
+}
